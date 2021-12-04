a4622c84c87092c6b83c94355a1ffc7e1ed3bbe1
John Wang <jzw2@illinois.edu>
2021-12-04 15:58:15 -0600

    Progress

--- src/ast.rs
+++ src/ast.rs
@@ -54,8 +54,8 @@ enum Stmt {
     StmtBlock(Box<Block>),
     Assign(String, Box<AExp>),
     Sequence(Box<Stmt>, Box<Stmt>),
-    IfThenElse(BExp, Block, Block),
-    While(BExp, Block),
+    IfThenElse(Box<BExp>, Box<Block>, Box<Block>),
+    While(Box<BExp>, Box<Block>),
 }
 
 #[derive(Clone, Debug)]
@@ -73,8 +73,19 @@ pub struct Stack {
 
 impl Stack {
     pub fn new() -> Stack {
+        let variables = vec!["x".to_string(), "y".to_string()];
+        let assign_x = Stmt::Assign("x".to_string(), Box::new(AExp::Int(5)));
+        let assign_y = Stmt::Assign("y".to_string(), Box::new(AExp::Int(7)));
+        let evaluate_x = AExp::Id("x".to_string());
+        let set_y_to_x = Stmt::Assign("y".to_string(), Box::new(evaluate_x));
+
+        let program = 
+          Stmt::Sequence(assign_x.into(), Box::new(Stmt::Sequence(
+              Box::new(assign_y),
+              Box::new(set_y_to_x)
+          )));
     Stack { stack: vec![Configuration::PgmConf(
-        Box::new(Pgm::Program(vec!["x".to_string()], Stmt::Assign("x".to_string(), Box::new(AExp::Int(5)))))
+        Box::new(Pgm::Program(variables, program))
         )], rules: vec![] }
     }
     // true means a sucess apply, false failed to apply rule
@@ -138,28 +149,43 @@ pub enum Rule {
     RewriteDivideLeft,
     //  crl o < A1 / A2,Sigma > => < A1 / A2',Sigma > if o < A2,Sigma > => < A2',Sigma > .
     RewriteDivideRight,
-
- // crl o < I1 / I2,Sigma > => < I1 /Int I2,Sigma > if I2 =/=Bool 0 .
+    // crl o < I1 / I2,Sigma > => < I1 /Int I2,Sigma > if I2 =/=Bool 0 .
     RewriteDivide,
- // crl o < A1 <= A2,Sigma > => < A1' <= A2,Sigma > if o < A1,Sigma > => < A1',Sigma > .
- RewriteLessThanLeft,
- // crl o < I1 <= A2,Sigma > => < I1 <= A2',Sigma > if o < A2,Sigma > => < A2',Sigma > .
- RewriteLessThanRight,
-  // rl o < I1 <= I2,Sigma > => < I1 <=Int I2,Sigma > .
-  RewriteLessThan,
+    // crl o < A1 <= A2,Sigma > => < A1' <= A2,Sigma > if o < A1,Sigma > => < A1',Sigma > .
+    RewriteLessThanLeft,
+    // crl o < I1 <= A2,Sigma > => < I1 <= A2',Sigma > if o < A2,Sigma > => < A2',Sigma > .
+    RewriteLessThanRight,
+    // rl o < I1 <= I2,Sigma > => < I1 <=Int I2,Sigma > .
+    RewriteLessThan,
+    // crl o < ! B,Sigma > => < ! B',Sigma > if o < B,Sigma > => < B',Sigma > .
+    RewriteNegate, 
+    // rl o < ! true,Sigma > => < false,Sigma > .
+    RewriteNegateTrue, 
 
-  
-
-    // "rl o < {} S2,Sigma > => < S2,Sigma > ."
-    RewriteEmptyBlock,
-
-
-    // "crl o < S1 S2,Sigma > => < S1' S2,Sigma' > if o < S1,Sigma > => < S1',Sigma' > ."
-    RewriteSequence,
+    // rl o < ! false,Sigma > => < true,Sigma > .
+    RewriteNegateFalse,
+    //   rl o < {S},Sigma > => < S,Sigma > .
+    RewriteBlockStatement,
+ 
     // "crl o < X = A ;,Sigma > => < X = A' ;,Sigma > if o < A,Sigma > => < A',Sigma > ."
     RewriteAssignmentArith,
     // Enum::Fourth => "crl o < X = I ;,Sigma > => < {},Sigma[I / X] > if Sigma(X) =/=Bool undefined .".to_string(),
     RewriteAssignmentInt,
+
+    // "crl o < S1 S2,Sigma > => < S1' S2,Sigma' > if o < S1,Sigma > => < S1',Sigma' > ."
+    RewriteSequence,
+    // "rl o < {} S2,Sigma > => < S2,Sigma > ."
+    RewriteEmptyBlock,
+
+    // crl o < if (B) S1 else S2,Sigma > => < if (B') S1 else S2,Sigma > if o < B,Sigma > => < B',Sigma  > .
+    RewriteConditional, 
+    // rl o < if (true) S1 else S2,Sigma > => < S1,Sigma > .
+    RewriteConditionalTrue,
+    // rl o < if (false) S1 else S2,Sigma > => < S2,Sigma > .
+    RewriteConditonalFalse,
+    // rl o < while (B) S,Sigma > => < if (B) {S while (B) S} else {},Sigma > .
+    RewriteLoop, 
+
     // Enum::Fifth => " o < int Xl ; S > => < S,(Xl |-> 0) > .".to_string(),
     RewriteTop,
     // Enum::Sixth => "None selected".to_string(),
@@ -173,7 +199,7 @@ impl Rule {
     fn get_next_configuration(&self, conf: Configuration) -> Option<Configuration> {
         let ret = match self {
             Rule::RewritePlus => {
-                return None
+                Configuration::Dummy
             }
             Rule::RewritePlusLeft => {
                  //crl o < A1 + A2,Sigma > => < A1' + A2,Sigma > if o < A1,Sigma > => < A1',Sigma > .
@@ -246,6 +272,56 @@ impl Rule {
                     _ => return None
                 }
             }
+            // crl o < A1 <= A2,Sigma > => < A1' <= A2,Sigma > if o < A1,Sigma > => < A1',Sigma > .
+            Rule::RewriteLessThanLeft => {
+                match conf {
+                    Configuration::BExpConf(x, sigma) =>
+                    match *x {
+                        BExp::LessThanEq(a1, _a2) => Configuration::AExpConf(a1, sigma),
+                        _ => return None
+                    }
+                    _ => return None
+                }
+            }
+            // crl o < I1 <= A2,Sigma > => < I1 <= A2',Sigma > if o < A2,Sigma > => < A2',Sigma > .
+            Rule::RewriteLessThanRight => {
+                // match conf {
+                //     Configuration::BExpConf(x, sigma) =>
+                //     match *x {
+                //         BExp::LessThanEq(a1, a2) => Configuration::AExpConf(a2, sigma),
+                //         _ => return None
+                //     }
+                //     _ => return None
+                // }
+                Configuration::Dummy
+            }
+            // rl o < I1 <= I2,Sigma > => < I1 <=Int I2,Sigma > .
+            Rule::RewriteLessThan => {
+                Configuration::Dummy
+            }
+             // crl o < ! B,Sigma > => < ! B',Sigma > if o < B,Sigma > => < B',Sigma > .
+            Rule::RewriteNegate => {
+                match conf {
+                    Configuration::BExpConf(x, sigma) =>
+                    match *x {
+                        BExp::Negation(b) => Configuration::BExpConf(b, sigma),
+                        _ => return None,
+                    }
+                    _ => return None,
+                }
+            }
+            // rl o < ! true,Sigma > => < false,Sigma > .
+            Rule::RewriteNegateTrue => {
+                Configuration::Dummy
+            }
+            // rl o < ! false,Sigma > => < true,Sigma > .
+            Rule:: RewriteNegateFalse => {
+                Configuration::Dummy
+            }
+            //   rl o < {S},Sigma > => < S,Sigma > .
+            Rule::RewriteBlockStatement => {
+                Configuration::Dummy
+            }
             Rule::RewriteEmptyBlock => {
                 // 
                 return None;
@@ -283,6 +359,26 @@ impl Rule {
 
                 Configuration::Dummy
             }
+            Rule::RewriteConditional => {
+                // crl o < if (B) S1 else S2,Sigma > => < if (B') S1 else S2,Sigma > if o < B,Sigma > => < B',Sigma  > .
+                match conf {
+                    Configuration::StmtConf(s, sigm) => {
+                        match *s {
+                            Stmt::IfThenElse(b_ptr, s1_ptr, s2_ptr) => {
+                                Configuration::BExpConf(b_ptr, sigm)
+                            }
+                            _ => return None, 
+                        }
+                    }
+                    _ => return None, 
+                }
+            }
+    // rl o < if (true) S1 else S2,Sigma > => < S1,Sigma > .
+            Rule::RewriteConditionalTrue => Configuration::Dummy,
+            // rl o < if (false) S1 else S2,Sigma > => < S2,Sigma > .
+            Rule::RewriteConditonalFalse => Configuration::Dummy,
+            // rl o < while (B) S,Sigma > => < if (B) {S while (B) S} else {},Sigma > .
+            Rule::RewriteLoop => Configuration::Dummy,
             _ => return None,
         };
         Some(ret)
@@ -292,6 +388,64 @@ impl Rule {
         
         let x = match self {
 
+            //  crl o < A1 / A2,Sigma > => < A1' / A2,Sigma > if o < A1,Sigma > => < A1',Sigma > .
+            Rule::RewriteDivideLeft => {
+                todo!()
+            }
+            //  crl o < A1 / A2,Sigma > => < A1 / A2',Sigma > if o < A2,Sigma > => < A2',Sigma > .
+            Rule::RewriteDivideRight => {
+                todo!()
+            }
+            Rule::RewriteDivide => {
+                match bottom {
+                    Configuration::AExpConf(x, sigma) => {
+                        match *x.clone() {
+                            AExp::Divide(box1, box2) => {
+                                match *box1 {
+                                    AExp::Int(n1) => {
+                                        match *box2 {
+                                            AExp::Int(0) => Configuration::AExpConf(x, sigma),
+                                            AExp::Int(n2) => Configuration::AExpConf(Box::new(AExp::Int(n1/n2)), sigma),
+                                            _ => return None
+                                        }
+                                    }
+                                    _ => return None
+                                }
+                            }
+                            _ => return None
+                        }
+                    }
+                    _ => return None
+                } 
+            }
+
+            Rule::RewritePlus => {
+                match bottom {
+                    Configuration::AExpConf(x, sigma) =>
+                      match *x {
+                        AExp::Plus(box1, box2) => {
+                            match *box1 {
+                                AExp::Int(n1) => {
+                                    match *box2 {
+                                    AExp::Int(n2) => Configuration::AExpConf(Box::new(AExp::Int(n1 + n2)), sigma),
+                                    _ => return None
+                                    }
+                                }
+                                _ => return None
+                            }
+                        }
+                        _ => return None
+                    }
+                    _ => return None
+                }
+            }
+
+            Rule::RewriteLessThan => {
+                match bottom {
+                    Configuration::AExpConf(x, sigma) => return None,
+                    _ => return None
+                }
+            }
 
             Rule::RewriteVariableLookup => {
                 // o < X,Sigma > => < Sigma(X),Sigma > if Sigma(X) =/=Bool undefined .
@@ -310,6 +464,19 @@ impl Rule {
                       },
                     _ => return None
                 }
+            }
+            // crl o < ! B,Sigma > => < ! B',Sigma > if o < B,Sigma > => < B',Sigma > .
+            Rule::RewriteNegate => {
+                todo!()
+            }
+            // rl o < ! true,Sigma > => < false,Sigma > .
+            Rule::RewriteNegateTrue => {
+                todo!()
+            }
+
+            // rl o < ! false,Sigma > => < true,Sigma > .
+            Rule::RewriteNegateFalse => {
+                todo!()
             }
             Rule::RewriteSequence => {
                 // "crl o < S1 S2,Sigma > => < S1' S2,Sigma' > if o < S1,Sigma > => < S1',Sigma' > ."
@@ -399,6 +566,88 @@ impl Rule {
                         }   
                     }
                     _ => return None,
+                }
+            }
+            // crl o < if (B) S1 else S2,Sigma > => < if (B') S1 else S2,Sigma > if o < B,Sigma > => < B',Sigma  > .
+            Rule::RewriteConditional => {
+                let new_bool = match top {
+                    Configuration::BExpConf(b, sigma) => b.clone(),
+                    _ => return None,
+                };
+                match bottom {
+                    Configuration::StmtConf(s, sigm) => {
+                        match *s {
+                            Stmt::IfThenElse(b_ptr, s1_ptr, s2_ptr) => {
+                                match *b_ptr {
+                                    BExp::Bool(true) => 
+                                      Configuration::StmtConf(
+                                          Stmt::IfThenElse(new_bool, s1_ptr, s2_ptr).into(), sigm),
+                                    _ => return None, 
+
+                                }
+                            }
+                            _ => return None, 
+                        }
+                    }
+                    _ => return None, 
+                }
+            }
+            Rule::RewriteConditonalFalse => {
+            // rl o < if (true) S1 else S2,Sigma > => < S1,Sigma > .
+                match bottom {
+                    Configuration::StmtConf(s, sigm) => {
+                        match *s {
+                            Stmt::IfThenElse(b_ptr, s1_ptr, s2_ptr) => {
+                                match *b_ptr {
+                                    BExp::Bool(true) => 
+                                      Configuration::StmtConf(Box::new(Stmt::StmtBlock(s1_ptr)), sigm),
+                                    _ => return None, 
+
+                                }
+                            }
+                            _ => return None, 
+                        }
+                    }
+                    _ => return None, 
+                }
+            }
+            // rl o < if (false) S1 else S2,Sigma > => < S2,Sigma > .
+            Rule::RewriteConditonalFalse => {
+                match bottom {
+                    Configuration::StmtConf(s, sigm) => {
+                        match *s {
+                            Stmt::IfThenElse(b_ptr, s1_ptr, s2_ptr) => {
+                                match *b_ptr {
+                                    BExp::Bool(false) => Configuration::StmtConf(Stmt::StmtBlock(s2_ptr).into(), sigm),
+                                    _ => return None, 
+
+                                }
+                            }
+                            _ => return None, 
+                        }
+                    }
+                    _ => return None, 
+                }
+            }
+            // rl o < while (B) S,Sigma > => < if (B) {S while (B) S} else {},Sigma > .
+            Rule::RewriteLoop => {
+                match bottom {
+                    Configuration::StmtConf(s, sigm) => {
+                        match *s {
+                            Stmt::While(b_ptr, s_ptr) => {
+                                Configuration::StmtConf(
+                                    Stmt::IfThenElse(b_ptr.clone(), 
+                                        Block::BlockStmt(
+                                            Stmt::Sequence(
+                                                Stmt::StmtBlock(s_ptr.clone()).into(), 
+                                                Stmt::While(b_ptr, s_ptr).into()
+                                            ).into()).into(), 
+                                            Block::EmptyBlock.into()).into(), sigm)
+                            }
+                            _ => return None, 
+                        }
+                    }
+                    _ => return None, 
                 }
             }
             _ => return None,
